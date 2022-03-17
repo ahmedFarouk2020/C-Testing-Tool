c5b29ad8d7924664552082d5fc82def3cdb22915
Ahmed Farouk <ahmedfarook0111@gmail.com>
2022-03-17 22:32:04 +0200

    clean the code

--- example/example.c
+++ example/example.c
@@ -3,20 +3,9 @@
 #include <stdlib.h>
 #include "../lib/testing.h"
 
-
-
 /*
  * This code is for testing a simple function using C language 
  */
- 
-int factorial(int number) {
-	int result = 1;
-	for(int i=2; i<=number; i++) {
-		result *= i;
-	}
-	
-	return result;
-}
 
 int sum(int x, int y) {
 	return x+y;
@@ -36,7 +25,7 @@ int main()
 	// Example: 
 	TEST((int)(123.315), (int)(float_()), Test_float, float);
 	TEST(4,     		 (sum(2,2)),	  Test_int,   int);
-	TEST("12",			 (str()), 		  Test_str,   str);
+	TEST("ahmed",			 (str()), 		  Test_str,   str);
 
 	getchar();
 }
